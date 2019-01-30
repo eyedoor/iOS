6f85000d645b4db65ea472e7f3451d50afbdb355
schlnate <nasdog07@gmail.com>
2019-01-30 15:53:43 -0600

    Initial Commit

---
+++ eyeDoor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  eyeDoor
+//
+//  Created by Nathan Schlechte on 1/30/19.
+//  Copyright © 2019 Nathan Schlechte. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
