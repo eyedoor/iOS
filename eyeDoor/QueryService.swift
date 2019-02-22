317f5676b70049762f8d1334bd93eef6623471ad
schlnate <nasdog07@gmail.com>
2019-02-22 13:10:03 -0600

    initial createUser api call

---
+++ eyeDoor/QueryService.swift
@@ -0,0 +1,68 @@
+//
+//  QueryService.swift
+//  eyeDoor
+//
+//  Created by Nathan Schlechte on 2/19/19.
+//  Copyright © 2019 Nathan Schlechte. All rights reserved.
+//
+
+import Foundation
+
+class QueryService {
+    
+    func creatUser(email: String, password: String, firstname: String, lastname: String){
+        let url = URL(string: "https://joseph-frank.com/api/users")!
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        let parameters: [String:Any] = [
+            "email" : email,
+            "password" : password,
+            "firstname" : firstname,
+            "lastname" : lastname
+        ]
+        request.httpBody = parameters.percentEscaped().data(using: .utf8)
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, let response = response as? HTTPURLResponse,
+                error == nil else {
+                    print("error", error ?? "unknown error")
+                    return
+            }
+            
+            guard (200 ... 299) ~= response.statusCode else {
+                print("statusCode should be 2xx, but is \(response.statusCode)")
+                print("response = \(response)")
+                return
+            }
+            
+            let responseString = String(data: data, encoding: .utf8)
+            print("responseString = \(responseString)")
+            
+        }
+        task.resume()
+        
+    }
+    
+}
+
+extension Dictionary {
+    func percentEscaped() -> String {
+        return map { (key, value) in
+            let escapedKey = "\(key)".addingPercentEncoding(withAllowedCharacters: .urlQueryValueAllowed) ?? ""
+            let escapedValue = "\(value)".addingPercentEncoding(withAllowedCharacters: .urlQueryValueAllowed) ?? ""
+            return escapedKey + "=" + escapedValue
+            }
+            .joined(separator: "&")
+    }
+}
+
+extension CharacterSet {
+    static let urlQueryValueAllowed: CharacterSet = {
+        let generalDelimitersToEncode = ":#[]@" // does not include "?" or "/" due to RFC 3986 - Section 3.4
+        let subDelimitersToEncode = "!$&'()*+,;="
+        
+        var allowed = CharacterSet.urlQueryAllowed
+        allowed.remove(charactersIn: "\(generalDelimitersToEncode)\(subDelimitersToEncode)")
+        return allowed
+    }()
+}
